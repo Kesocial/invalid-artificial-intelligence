99bbd80b8defb48c333946d0ff1b75f4b47496be
Kevin Daniel Acevedo <kacevedo@aplay.com.ar>
2023-02-28 02:15:40 -0300

    fix utils

--- utils.js
+++ utils.js
@@ -1,103 +1,49 @@
-import { capitalize } from './utils.js';
+import fetch from 'node-fetch';
+import { verifyKey } from 'discord-interactions';
 
-export function getResult(p1, p2) {
-  let gameResult;
-  if (RPSChoices[p1.objectName] && RPSChoices[p1.objectName][p2.objectName]) {
-    // o1 wins
-    gameResult = {
-      win: p1,
-      lose: p2,
-      verb: RPSChoices[p1.objectName][p2.objectName],
-    };
-  } else if (
-    RPSChoices[p2.objectName] &&
-    RPSChoices[p2.objectName][p1.objectName]
-  ) {
-    // o2 wins
-    gameResult = {
-      win: p2,
-      lose: p1,
-      verb: RPSChoices[p2.objectName][p1.objectName],
-    };
-  } else {
-    // tie -- win/lose don't
-    gameResult = { win: p1, lose: p2, verb: 'tie' };
+export function VerifyDiscordRequest(clientKey) {
+  return function (req, res, buf, encoding) {
+    const signature = req.get('X-Signature-Ed25519');
+    const timestamp = req.get('X-Signature-Timestamp');
+
+    const isValidRequest = verifyKey(buf, signature, timestamp, clientKey);
+    if (!isValidRequest) {
+      res.status(401).send('Bad request signature');
+      throw new Error('Bad request signature');
+    }
+  };
+}
+
+export async function DiscordRequest(endpoint, options) {
+  // append endpoint to root API URL
+  const url = 'https://discord.com/api/v10/' + endpoint;
+  // Stringify payloads
+  if (options.body) options.body = JSON.stringify(options.body);
+  // Use node-fetch to make requests
+  const res = await fetch(url, {
+    headers: {
+      Authorization: `Bot ${process.env.DISCORD_TOKEN}`,
+      'Content-Type': 'application/json; charset=UTF-8',
+      'User-Agent': 'DiscordBot (https://github.com/discord/discord-example-app, 1.0.0)'
+    },
+    ...options
+  });
+  // throw API errors
+  if (!res.ok) {
+    const data = await res.json();
+    console.log(res.status);
+    throw new Error(JSON.stringify(data));
   }
-
-  return formatResult(gameResult);
+  // return original response
+  return res;
 }
 
-function formatResult(result) {
-  const { win, lose, verb } = result;
-  return verb === 'tie'
-    ? `<@${win.id}> and <@${lose.id}> draw with **${win.objectName}**`
-    : `<@${win.id}>'s **${win.objectName}** ${verb} <@${lose.id}>'s **${lose.objectName}**`;
+// Simple method that returns a random emoji from list
+export function getRandomEmoji() {
+  const emojiList = ['😭','😄','😌','🤓','😎','😤','🤖','😶‍🌫️','🌏','📸','💿','👋','🌊','✨'];
+  return emojiList[Math.floor(Math.random() * emojiList.length)];
 }
 
-// this is just to figure out winner + verb
-const RPSChoices = {
-  rock: {
-    description: 'sedimentary, igneous, or perhaps even metamorphic',
-    virus: 'outwaits',
-    computer: 'smashes',
-    scissors: 'crushes',
-  },
-  cowboy: {
-    description: 'yeehaw~',
-    scissors: 'puts away',
-    wumpus: 'lassos',
-    rock: 'steel-toe kicks',
-  },
-  scissors: {
-    description: 'careful ! sharp ! edges !!',
-    paper: 'cuts',
-    computer: 'cuts cord of',
-    virus: 'cuts DNA of',
-  },
-  virus: {
-    description: 'genetic mutation, malware, or something inbetween',
-    cowboy: 'infects',
-    computer: 'corrupts',
-    wumpus: 'infects',
-  },
-  computer: {
-    description: 'beep boop beep bzzrrhggggg',
-    cowboy: 'overwhelms',
-    paper: 'uninstalls firmware for',
-    wumpus: 'deletes assets for',
-  },
-  wumpus: {
-    description: 'the purple Discord fella',
-    paper: 'draws picture on',
-    rock: 'paints cute face on',
-    scissors: 'admires own reflection in',
-  },
-  paper: {
-    description: 'versatile and iconic',
-    virus: 'ignores',
-    cowboy: 'gives papercut to',
-    rock: 'covers',
-  },
-};
-
-export function getRPSChoices() {
-  return Object.keys(RPSChoices);
-}
-
-// Function to fetch shuffled options for select menu
-export function getShuffledOptions() {
-  const allChoices = getRPSChoices();
-  const options = [];
-
-  for (let c of allChoices) {
-    // Formatted for select menus
-    // https://discord.com/developers/docs/interactions/message-components#select-menu-object-select-option-structure
-    options.push({
-      label: capitalize(c),
-      value: c.toLowerCase(),
-      description: RPSChoices[c]['description'],
-    });
-  }
-
-  return options.sort(() => Math.random() - 0.5);
+export function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
