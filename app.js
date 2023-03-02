3d6f000c4d39f566fb722fdfdccfd84f751b1692
Kevin Daniel Acevedo <kacevedo@aplay.com.ar>
2023-03-02 13:03:45 -0300

    cache

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 import 'dotenv/config';
 import { REST, Routes } from 'discord.js'
 import { COMMANDS } from './commands.js';
-
+const commands = [
+  {
+    name: 'test',
+    description: 'Say hi to your app2!',
+  },
+  {
+    name: 'ping',
+    description: 'Replies with Pong2!',
+   
+  },
+  {
+    name: 'play',
+    description: 'For play music!2',
+   
+  }
+];
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 (async () => {
 	try {
 		console.log('Started refreshing application (/) commands.');
 
-		await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: COMMANDS});
+		await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands});
 
 		console.log('Successfully reloaded application (/) commands.');
 	} catch (error) {
@@ -26,7 +41,6 @@ client.on("error",async(error)=>{
 	console.log(error)
 })
 client.on('interactionCreate', async (interaction) => {
-	if (!interaction.isChatInputCommand()) return; 
 	if (interaction.commandName="test") {
 		await interaction.reply('Hello World!');
 	}
