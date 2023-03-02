2ccb12c65d90c87f225cca8b9b5c726f7dbe4dfa
Kevin Daniel Acevedo <kacevedo@aplay.com.ar>
2023-03-02 12:49:33 -0300

    cache

--- commands.js
+++ commands.js
@@ -1,26 +1,19 @@
 import { capitalize } from './utils.js';
 
 // Simple test command
-export const COMMANDS = {
-  "test":{
+export const COMMANDS = [
+  {
     name: 'test',
     description: 'Say hi to your app!',
-    do:async()=>{ 
-		  await interaction.reply('Hello World!');
-    }
   },
-  "ping":{
+  {
     name: 'ping',
     description: 'Replies with Pong!',
-    do:async()=>{
-		  await interaction.reply('Pong!');
-    }
+   
   },
-  "play":{
+  {
     name: 'play',
     description: 'For play music!',
-    do:async()=>{
-		  await interaction.reply('Coming soon...');
-    }
+   
   }
-};
+];
