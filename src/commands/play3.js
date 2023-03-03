8d599d64e58ba3c41795bd291f0d0c6e7d4c0e6b
Kevin Daniel Acevedo <kacevedo@aplay.com.ar>
2023-03-03 11:02:56 -0300

    a

--- src/commands/play3.js
+++ src/commands/play3.js
@@ -1,14 +1,11 @@
 import {joinVoiceChannel} from "@discordjs/voice"
 const execute = async (interaction)=>{
   const channel = interaction.member.voice.channel
-  const user = interaction.member.user
-  console.log({channel})
-  console.log({user})
-  // joinVoiceChannel({
-  //   channelId:channel.id,
-  //   guildId:channel.guild.id,
-  //   adapterCreator:channel.guild.voiceAdapterCreator
-  // })
+  joinVoiceChannel({
+    channelId:channel.id,
+    guildId:channel.guild.id,
+    adapterCreator:channel.guild.voiceAdapterCreator
+  })
   await interaction.reply('Coming Soon!');
 }
 // Simple test command
