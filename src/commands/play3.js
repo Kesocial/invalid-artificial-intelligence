873969482142ec356381e18782c4ffbeca520037
Kevin Daniel Acevedo <kacevedo@aplay.com.ar>
2023-03-03 10:33:16 -0300

    a

--- src/commands/play3.js
+++ src/commands/play3.js
@@ -2,7 +2,7 @@ import {joinVoiceChannel} from "@discordjs/voice"
 const execute = async (interaction)=>{
   console.log({channelid:interaction.channelId})
   console.log({guildid:interaction.guildId})
-  console.log({vcStates:interaction.guild})
+  console.log({vcStates:interaction.guild.voiceStates})
   // joinVoiceChannel({
   //   channelId:channel.id,
   //   guildId:channel.guild.id,
